54dce43aa787b46acb78a89c706c5313d7b4906d
Betsy Sallee <ensallee@gmail.com>
2018-01-31 18:52:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,7 +6,7 @@ function getIt(){
 }
 function frameIt(){
   $('img').on('load', function(){
-    return $('img').append('.tasty');
+    return $('img').bind('.tasty');
   });
 }
 function pressIt(){
